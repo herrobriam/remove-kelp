5dadede1f1191a1aba780ed223baf536c896b481
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-06-23 16:27:48 +0100

    Initial commit

---
+++ src/main/java/net/herrobriam/removekelp/RemoveKelpDataGenerator.java
@@ -0,0 +1,11 @@
+package net.herrobriam.removekelp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RemoveKelpDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
